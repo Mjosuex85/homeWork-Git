2769a97b4e23c2f1b0e6481b1d3fdae0237ad74c
aalred <aalex.red1@gmail.com>
2022-08-27 10:03:52 -0500

    feat(index): added function
    returns hexa or bin values

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
-// intento 34
+// intento 334
+    if (numero%2===0 ) {
+        return numero.toString(2)
+    } else if (numero%2===1) {   
+        return numero.toString(16).toUpperCase()
+    }
 }
